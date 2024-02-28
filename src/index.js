10a1df77968ef12f9ff96d3f6d5b5b6c76b2677a
Anthony Andrango <sir-ankun>
2024-02-28 12:56:52 -0500

    Implementación de la iteración 3, 4

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:4200" }));
+app.use(cors({ origin: "https://gestion-formularios-mcevallos.netlify.app" }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
