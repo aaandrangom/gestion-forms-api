16bf29cc9c011bd271eb19680c37593ace3dfbee
Anthony Andrango <sir-ankun>
2024-03-06 08:24:57 -0500

    Implementación de recuperar contraseña, numeros decimales en un formulario, subir plantilla actualizada

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:4200" }));
+app.use(cors({ origin: process.env.CLIENT_URL }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
